a8d79f0d6b3f0694592dbbc7044dbc27dc35d3e6
Catriona Watson <catrionahw@gmail.com>
2017-09-12 10:47:02 +0100

    Finished customer domain class

--- src/main/java/io/zipcoder/domain/Customer.java
+++ src/main/java/io/zipcoder/domain/Customer.java
@@ -1,8 +1,45 @@
 package io.zipcoder.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Set;
 
 @Entity
+
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String first_name;
+    private String last_name;
+    private Set<Address> address;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Set<Address> getAddress() {
+        return address;
+    }
 
 }
