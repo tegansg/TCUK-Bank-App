1bc444d16cb5a58a11c2e712730a08c9ec9d1053
Issy Irvine <issyirvine@gmail.com>
2017-09-14 10:00:35 +0100

    clean up code

--- src/main/java/io/zipcoder/controller/AddressController.java
+++
@@ -1,8 +0,0 @@
-package io.zipcoder.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AddressController {
-
-}
--- src/main/java/io/zipcoder/repositories/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package io.zipcoder.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import io.zipcoder.domain.Address;
-
-public interface AddressRepository extends CrudRepository<Address, Long>{
-
-}
